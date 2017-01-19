1dfc840efd52c1e123e1540122e547c40ae3a37c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-01-19 22:29:02 +0100

    Use WebGL canvas (black square)

--- hw.ts
+++ hw.ts
@@ -1,1 +1,10 @@
-console.log("hello, world");
+function start() {
+    let canvas = <HTMLCanvasElement>document.getElementById('glcanvas');
+    let gl = <WebGLRenderingContext>canvas.getContext('webgl');
+    if (!gl) alert("can't get GL context");
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+start();
